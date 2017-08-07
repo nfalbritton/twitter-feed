1f676776f3b0816702b69f96cf2f24f05b006247
Nadia Fermin Albritton <ferminnadia@gmail.com>
2017-08-07 16:58:40 -0400

    added some foundation

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,18 +3,38 @@ import React from 'react';
 const Tweet = props => {
   let retweet;
   if(props.retweeted) {
-    retweet = <button className='retweet' onClick={event => alert('retweet')}>{props.retweetCount}</button>
+    retweet = <button className='fa fa-retweet icon green' aria-hidden='true' onClick={event => alert('retweet')}>{props.retweetCount}</button>
   };
 
   let favorite;
   if(props.favorited) {
-    favorite = <button className='favorite' onClick={event => alert('favorited')}>{props.favoriteCount}</button>
+    favorite = <button className='fa fa-heart icon red' aria-hidden='true' onClick={event => alert('favorited')}>{props.favoriteCount}</button>
   };
 
-
   return(
-    <div className="alerts">
+    <div>
+      <li>
+        <div className="pic columns small-2">
+          <img src={props.profileImage} />
+        </div>
 
-  )
+        <div className="names column small-8">
+          <div className="text row small-12">
+            <span className="name">{props.name}</span>
+            <span className="screenName">{props.screenName}</span>
+          </div>
+          <span className="text">{props.text}</span>
+
+        </div>
+
+        <div className="likes row small-2">
+          <span className="retweet">{retweet}</span>
+          <span className="favorite">{favorite}</span>
+        </div>
+      </li>
+    </div>
+  );
 }
-export default Tweet;
+
+
+    export default Tweet;
